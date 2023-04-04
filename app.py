a5102b441be278e0587b0f62fe2ab6e7dfaedb9d
Graziela França <38873168+Grazielafranca31@users.noreply.github.com>
2023-04-04 11:00:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ from scraper import coleta
 TELEGRAM_API_KEY = os.environ['TELEGRAM_API_KEY']
 TELEGRAM_ADMIN_ID = os.environ['TELEGRAM_ADMIN_ID']
 GOOGLE_SHEETS_CREDENTIALS = os.environ['GOOGLE_SHEETS_CREDENTIALS']
-conta = ServiceAccountCredentials.from_json_keyfile_dict(GOOGLE_SHEETS_CREDENTIALS)
+with open("credenciais.json", mode="w") as arquivo:
+  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
 api = gspread.authorize(conta)
 planilha = api.open_by_key(GOOGLE_SHEETS_KEY)
 sheet = planilha.worksheet("Página1")
 app = Flask(__name__)
+
 
 
 @app.route("/")
