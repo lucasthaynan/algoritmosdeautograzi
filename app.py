e50a5be6dabfcd8f1516275e2693d1f379161105
Graziela França <38873168+Grazielafranca31@users.noreply.github.com>
2023-04-04 00:46:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,16 @@ from flask import Flask, request
 from oauth2client.service_account import ServiceAccountCredentials
 from tchan import ChannelScraper
 
-TELEGRAM_API_KEY=os.environ['TELEGRAM_API_KEY']
-TELEGRAM_ADMIN_ID=os.environ['TELEGRAM_ADMIN_ID']
-GOOGLE_SHEETS_CREDENTIALS=os.environ['GOOGLE_SHEETS_CREDENTIALS']
-GOOGLE_SHEETS_CREDENTIALS=os.environ['GOOGLE_SHEETS_CREDENTIALS']
-with open('credenciais.json', mode='w') as arquivo:
-  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
-conta=ServiceAccountCredentials.from_json_keyfile_name('credenciais.json')
+TELEGRAM_API_KEY = os.environ['TELEGRAM_API_KEY']
+TELEGRAM_ADMIN_ID = os.environ['TELEGRAM_ADMIN_ID']
+GOOGLE_SHEETS_CREDENTIALS = os.environ['GOOGLE_SHEETS_CREDENTIALS']
+
+conta = ServiceAccountCredentials.from_json_keyfile_dict(GOOGLE_SHEETS_CREDENTIALS)
 api = gspread.authorize(conta)
-planilha = api.open_by_key(f'{GOOGLE_SHEETS_KEY}') 
+planilha = api.open_by_key(GOOGLE_SHEETS_KEY)
 sheet = planilha.worksheet("Página1")
 app = Flask(__name__)
+
 
 @app.route("/")
 # Função para coletar notícias do site da Agência Tatu
